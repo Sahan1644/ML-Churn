b4f31ec1fb19ad1c93f80455c357762d8ddc1a4a
Sahan1644 <sahan.20240855@iit.ac.lk>
2025-12-03 18:57:11 +0530

    Changes to model_dt.py

--- src/model_dt.py
+++ src/model_dt.py
@@ -1,9 +1,10 @@
+# src/model_dt.py
 from sklearn.tree import DecisionTreeClassifier
+import pickle
 
 def train_dt(X_train, y_train):
-    dt = DecisionTreeClassifier(random_state=42)
+    dt = DecisionTreeClassifier(max_depth=5, random_state=42)
     dt.fit(X_train, y_train)
+    with open("src/dt_model.pkl", "wb") as f:
+        pickle.dump(dt, f)
     return dt
-
-def predict_dt(dt, X):
-    return dt.predict(X)
